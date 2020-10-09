271bed385284ed4298c5073d8bd5a21fa567e77a
Olivier <rodomond.o@gmail.com>
2020-10-09 22:18:08 +0700

    By pass the main page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,66 +23,27 @@ module.exports = {
       },
       items: [
         {
+          href: 'https://restqa.io',
+          label: 'Products',
+          position: 'right',
+        }, {
           to: 'docs/',
           activeBasePath: 'docs',
           label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/restqa',
-          label: 'GitHub',
+          position: 'right',
+        }, {
+          to: 'blog',
+          label: 'Blog',
+          position: 'right',
+        }, {
+          href: 'https://restqa.io/#/contact',
+          label: 'Contact Us',
           position: 'right',
         },
       ],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
       copyright: `Copyright © ${new Date().getFullYear()} RestQA, Inc. Built with Docusaurus.`,
     },
   },
