3f36076cf2a7ef9be5e6c2479043c3810acfe493
Olivier <hi@olivierodo.me>
2021-03-12 16:24:26 +0700

    Change doc import strategy

--- plugins.js
+++ plugins.js
@@ -1,7 +1,10 @@
 module.exports = [{
   name: "RestQapi Plugin",
+  enabled: true,
   repository: "",
   docs: [{
+    output: "tests/plugins/config-restqapi.md"
+  },{
     input: "https://raw.githubusercontent.com/restqa/restqapi/master/docs/features.md",
     output: "tests/restqapi/features.md",
     metadata: {
@@ -31,6 +34,47 @@ module.exports = [{
   },{
     input: "https://raw.githubusercontent.com/restqa/restqapi/master/src/restqapi/steps/3-then/index.js",
     output: "tests/restqapi/then.md",
+    metadata: {
+      id: 'then',
+      sidebar_label: 'Then',
+      title: 'Then: Describe an expected outcome'
+    },
+    jsdoc: true
+  }]
+},{
+  name: "RestQkube Plugin",
+  enabled: false,
+  repository: "",
+  docs: [{
+    input: "https://raw.githubusercontent.com/restqa/restqkube/master/docs/features.md",
+    output: "tests/restqkube/features.md",
+    metadata: {
+      id: 'features',
+      sidebar_label: 'Gherkin References',
+      title: 'Gherkin References'
+    },
+    jsdoc: false
+  },{
+    input: "https://raw.githubusercontent.com/restqa/restqkube/master/src/restqkube/steps/1-given/index.js",
+    output: "tests/restqkube/given.md",
+    metadata: {
+      id: 'given',
+      sidebar_label: 'Given',
+      title: 'Given: Describe your context'
+    },
+    jsdoc: true
+  },{
+    input: "https://raw.githubusercontent.com/restqa/restqkube/master/src/restqkube/steps/2-when/index.js",
+    output: "tests/restqkube/when.md",
+    metadata: {
+      id: 'when',
+      sidebar_label: 'When',
+      title: 'When: Describe an event'
+    },
+    jsdoc: true
+  },{
+    input: "https://raw.githubusercontent.com/restqa/restqkube/master/src/restqkube/steps/3-then/index.js",
+    output: "tests/restqkube/then.md",
     metadata: {
       id: 'then',
       sidebar_label: 'Then',
